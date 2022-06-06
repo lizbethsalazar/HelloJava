2ea703a9f64c417285e5ca5e31ab344b80b5d271
Lizbeth Salazar <lizsa29@gmail.com>
2022-06-05 20:04:21 -0500

    Imprimir *

--- HelloJava.java
+++ HelloJava.java
@@ -1,5 +1,15 @@
 public class HelloJava {
     public static void main(String[] args) {
-        System.out.println("Hello Java");
+        int cant = 50;
+        for(int i=1;i<cant;i++)
+            {
+                
+                for(int j=0;j<i;j++){
+                    System.out.print("*");
+                }
+
+                System.out.println("");
+            }
+
     }
 }
